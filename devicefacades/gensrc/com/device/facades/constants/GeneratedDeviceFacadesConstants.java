4830669168b8d771375484783e352c0133f49f20
Ravi Gadela <ravi.gadela@neonstingray.com>
2016-09-21 14:39:05 +1000

    Initial Template Check

---
+++ devicefacades/gensrc/com/device/facades/constants/GeneratedDeviceFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 21/09/2016 12:57:17 PM                      ---
+ * ----------------------------------------------------------------
+ */
+package com.device.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedDeviceFacadesConstants
+{
+	public static final String EXTENSIONNAME = "devicefacades";
+	
+	protected GeneratedDeviceFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ deviceinitialdata/gensrc/com/device/initialdata/constants/GeneratedDeviceInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 21/09/2016 12:57:17 PM                      ---
+ * ----------------------------------------------------------------
+ */
+package com.device.initialdata.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedDeviceInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "deviceinitialdata";
+	
+	protected GeneratedDeviceInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
